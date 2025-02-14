f5f23f335334044a5bf1504d20baa561b3284698
miskaldukhi <miskaldukhi@gmail.com>
2025-02-14 23:18:21 +0300

    maximini done

---
+++ src/maximini.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val maximini = arrayListOf(2.5, 7.1, 3.8, 9.2, 5.6)
+    val maxi = maximini.maxOrNull()
+    val mini = maximini.minOrNull()
+
+    println("The Largest number: $maxi")
+    println("The Largest number: $mini")
+
+}
