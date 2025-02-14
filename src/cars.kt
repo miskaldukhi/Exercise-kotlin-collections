fa3f4027881d1dcbb8036be5daede925f623f93b
miskaldukhi <miskaldukhi@gmail.com>
2025-02-14 22:47:20 +0300

    cars done

---
+++ src/cars.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val carBrands = arrayListOf("BMW", "Ferrari", "Honda")
+    carBrands.add("Porsche")
+    carBrands.add("Hyundai")
+
+    carBrands.remove("Honda")
+    println(carBrands)
+}
